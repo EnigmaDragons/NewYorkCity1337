44737ac814c6fbc888bb38c84f9da9b44c8c206c
Silas Reinagel <silas.reinagel@gmail.com>
2017-02-16 22:57:08 -0700

    Added font

---
+++ NewYorkCity1337/NewYorkCity1337/Graphics/DefaultFont.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NewYorkCity1337.Engine;
+
+namespace NewYorkCity1337.Graphics
+{
+    public static class DefaultFont
+    {
+        public static SpriteFont Font { get; set; }
+
+        static DefaultFont()
+        {
+            Font = new GameInstance().Load<SpriteFont>("Arial");
+        }
+    }
+}
