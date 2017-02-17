cc9bd33c2af9129b4cb5c49224105e39b9c25db7
noah reinagel <noah.r9@gmail.com>
2017-02-16 22:16:32 -0700

    Halfway there

---
+++ NewYorkCity1337/NewYorkCity1337/Input/MouseInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYorkCity1337.Input
+{
+    class MouseInput
+    {
+    }
+}
