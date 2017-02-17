95f40e3fc99c8d3d1b9c8b98925b9d68da9d6564
Silas Reinagel <silas.reinagel@gmail.com>
2017-02-16 22:42:45 -0700

    Integrated Account and Earnings into Buildings

--- NewYorkCity1337/NewYorkCity1337/Buildings/BasicBuilding.cs
+++ NewYorkCity1337/NewYorkCity1337/Buildings/BasicBuilding.cs
@@ -1,23 +1,31 @@
-﻿using Graphics;
+﻿using System;
+using Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using NewYorkCity1337.Business;
 using NewYorkCity1337.Tiles;
 
 namespace NewYorkCity1337.Buildings
 {
     public class BasicBuilding : Building
     {
+        private TimeSpan _timeSinceLastPayment = new TimeSpan(0, 0, 0);
         private readonly string _spriteName;
+        private readonly int _income;
 
         private Texture2D _sprite;
+        private MoneyAccount _moneyAccount;
 
+        public int Price { get; }
         public string DisplayName { get; }
         public Texture2D DisplayImage => _sprite;
 
-        public BasicBuilding(string displayName, string spriteName)
+        public BasicBuilding(string displayName, string spriteName, int price, int income)
         {
             DisplayName = displayName;
             _spriteName = spriteName;
+            Price = price;
+            _income = income;
         }
 
         public void LoadContent()
@@ -32,11 +40,25 @@ namespace NewYorkCity1337.Buildings
 
         public void Update(GameTime deltaTime)
         {
+            _timeSinceLastPayment += deltaTime.ElapsedGameTime;
+            if (_moneyAccount == null)
+                return;
+
+            if (_timeSinceLastPayment >= TimeSpan.FromSeconds(5))
+            {
+                _moneyAccount?.Gain(_income);
+                _timeSinceLastPayment -= TimeSpan.FromSeconds(5);
+            }
         }
 
         public void Draw(TileLocation location)
         {
             new DrawOnTile(_sprite, location).Go();
+        }
+
+        public void AssignOwner(MoneyAccount ownerAccount)
+        {
+            _moneyAccount = ownerAccount;
         }
     }
 }
--- NewYorkCity1337/NewYorkCity1337/Buildings/Building.cs
+++ NewYorkCity1337/NewYorkCity1337/Buildings/Building.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using NewYorkCity1337.Business;
 using NewYorkCity1337.Engine;
+using NewYorkCity1337.NewFolder1;
 
 namespace NewYorkCity1337.Buildings
 {
@@ -7,5 +9,7 @@ namespace NewYorkCity1337.Buildings
     {
         string DisplayName { get; }
         Texture2D DisplayImage { get; }
+        int Price { get; }
+        void AssignOwner(MoneyAccount ownerAccount);
     }
 }
